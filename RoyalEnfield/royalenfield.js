c8455bf119b9499bb500d4b56253e0231b39177c
ankitajoshi060 <ankitajoshi060@gamil.com>
2024-01-13 09:53:32 +0530

    added royal enfield project

---
+++ RoyalEnfield/royalenfield.js
@@ -0,0 +1,78 @@
+function a() {
+    document.getElementById("kk").src="./images/111.avif"; 
+}
+
+function b(){
+    document.getElementById("kk").src="./images/112.avif";
+}
+
+function c(){
+    document.getElementById("kk").src="./images/13.avif";
+}
+
+function d()
+{
+    document.getElementById("kk").src="./images/14.avif";
+}
+
+function e(){
+    document.getElementById("kk").src="./images/15.avif";
+}
+
+function f(){
+    document.getElementById("kk").src="./images/16.avif" ;
+}
+
+function g(){
+    document.getElementById("kk").src="./images/17.avif" ;
+}
+
+function a1(){
+    document.getElementById("ll").src="./images/21.avif";
+}
+
+function b1(){
+    document.getElementById("ll").src="./images/22.avif";
+}
+
+function c1(){
+    document.getElementById("ll").src="./images/23.avif";
+}
+
+function d1(){
+    document.getElementById("ll").src="./images/24.avif";
+}
+function e1(){
+    document.getElementById("ll").src="./images/25.avif";
+}
+
+function a2(){
+    document.getElementById("mm").src="./images/31.avif";
+}
+
+function b2(){
+    document.getElementById("mm").src="./images/32.avif";
+}
+
+function c2(){
+    document.getElementById("mm").src="./images/33.avif";
+}
+
+function d2(){
+    document.getElementById("mm").src="./images/34.avif";
+}
+function e2(){
+    document.getElementById("mm").src="./images/35.avif";
+}
+
+function a3(){
+    document.getElementById("nn").src="./images/41.avif"
+}
+
+function a4(){
+    document.getElementById("oo").src="./images/51.avif";
+}
+
+function b4(){
+    document.getElementById("oo").src="./images/52.avif";
+}
